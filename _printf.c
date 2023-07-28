e27c4f82f9029003cc61b16180a8631fb11d17a6
Abdelhamid Maaira <hamid.maira709@gmail.com>
2023-07-28 22:58:52 +0100

    _printf fix3

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@
 
 int _printf(const char *format, ...)
 {
-	int count = 0;
+	unsigned int count = 0;
 	char c;
 	va_list args;
 
@@ -25,45 +25,27 @@ int _printf(const char *format, ...)
 			switch (c)
 			{
 				case 'c':
-					char ch = (char)va_arg(args, int);
-
-					count += print_char(ch);
+					count += print_char((char)va_arg(args, int));
 					break;
 				case 's':
-					const char *str = va_arg(args, const char*);
-
-					count += print_string(str);
+					count += print_string(va_arg(args, const char*));
 					break;
 				case '%':
 					count += print_char('%');
 					break;
 				case 'd':
 				case 'i':
-					int num = va_arg(args, int);
-
-					count += print_integer(num);
-					break;
 				case 'u':
-					unsigned int num = va_arg(args, unsigned int);
-
-					count += print_unsigned(num);
-					break;
 				case 'o':
-					unsigned int num = va_arg(args, unsigned int);
-
-					count += print_octal(num);
-					break;
 				case 'x':
 				case 'X':
-					unsigned int num = va_arg(args, unsigned int);
-					int uppercase = (c == 'X') ? 1 : 0;
-
-					count += print_hexadecimal(num, uppercase);
+					if (c == 'u' || c == 'o' || c == 'x' || c == 'X')
+						count += print_unsigned(va_arg(args, unsigned int));
+					else
+						count += print_integer(va_arg(args, int));
 					break;
 				case 'p':
-					void *ptr = va_arg(args, void*);
-
-					count += print_pointer(ptr);
+					count += print_pointer(va_arg(args, void*));
 					break;
 				default:
 					count += print_char('%');
@@ -72,5 +54,5 @@ int _printf(const char *format, ...)
 		}
 	}
 	va_end(args);
-	return (count);
+	return count;
 }
